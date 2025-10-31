b689d32d244a5f6973f805548b5974d9e4eb1923
ujjwalkca-blip <ujjwalk.ca@gmail.com>
2025-10-31 12:34:36 +0545

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,73 @@
+const CACHE_NAME = 'isa-mcq-cache-v1';
+const urlsToCache = [
+    './',
+    './index.html',
+    './styles.css',
+    './script.js',
+    './scripts/icai_functions.js',
+    './data/module1.json',
+    './data/module2.json',
+    './data/module3.json',
+    './data/module4.json',
+    './data/module5.json',
+    './data/module6.json',
+    './data/icai_review.json'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+
+                return fetch(event.request).then(
+                    (response) => {
+                        // Check if we received a valid response
+                        if(!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+
+                        // Clone the response
+                        const responseToCache = response.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then((cache) => {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return response;
+                    }
+                );
+            })
+    );
+});
+
+// Clean up old caches
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
